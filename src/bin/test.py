952f16ef020e37e0a66839ed484c23a185769def
subcrip <contact@subc.rip>
2024-04-17 16:42:01 +0800

    backup

--- src/bin/test.py
+++ src/bin/test.py
@@ -1,6 +1,19 @@
-from random import randint
-print(100000)
-for _ in range(2):
-    for i in range(100000):
-        print(randint(0, 1), end='')
-    print()
+from typing import DefaultDict
+
+
+n = int(input())
+a = [int(x) for x in input().strip().split(' ')]
+d = sorted(list(set(a)), reverse=True)
+a.sort()
+slot = DefaultDict(lambda: [])
+slot_cnt = DefaultDict(lambda: 0)
+for i in range(n):
+    for j in range(i + 1, n):
+        for k in d:
+            if a[i] % k == 0 and a[j] % k == 0:
+                slot[k].append((a[i], a[j]))
+                slot_cnt[k] += 1
+                break
+
+print(slot)
+print(n * (n - 1) // 2 - sum(v for v in slot_cnt.values()))
